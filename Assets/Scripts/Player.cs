43d55047a7a540f952aef0df5a223c460c4ec6d8
Nikita <shpulinn@mail.ru>
2022-06-12 11:42:59 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,47 +1,13 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class Player : MonoBehaviour
+public class Player : Mover
 {
-    [SerializeField] private float speed = 2.0f;
-    private BoxCollider2D _boxCollider;
-    private Vector3 _moveDelta;
-    private RaycastHit2D _hit;
-
-    private void Start()
-    {
-        _boxCollider = GetComponent<BoxCollider2D>();
-    }
-
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        
-        // Reset _moveDelta
-        _moveDelta = new Vector3(x, y, 0);
-        
-        // Swap sprite direction, whether you're going: right or left
-        if (_moveDelta.x > 0) 
-            transform.localScale = Vector3.one;
-        else if (_moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-        
-        // check raycast hit
-        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(0, _moveDelta.y),
-            Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (_hit.collider == null) 
-        {
-            // Moving
-            transform.Translate(0, _moveDelta.y * (Time.deltaTime * speed), 0);
-        }
-        
-        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new Vector2(_moveDelta.x, 0),
-            Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (_hit.collider == null) 
-        {
-            // Moving
-            transform.Translate(_moveDelta.x * (Time.deltaTime * speed), 0, 0);
-        }
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
