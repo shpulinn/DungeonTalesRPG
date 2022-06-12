500c9087d6c3692b3da5cfdde919d40209fba966
Nikita <shpulinn@mail.ru>
2022-06-12 11:43:38 +0200

    Fighter script created

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    // Public fields
+    public int hitPoint;
+    public int maxHitPoint = 10;
+    public float pushRecoverySpeed = 0.2f;
+    
+    // Immunity
+    protected float immuneTime = 1.0f;
+    protected float lastImmune;
+    
+    // Push
+    protected Vector3 pushDirection;
+    
+    // All fighters can ReceiveDamage or Dio
+    protected virtual void ReceiveDamage(Damage dmg)
+    {
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            hitPoint -= dmg.damageAmount;
+            pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+            
+            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.zero, 0.5f);
+
+            if (hitPoint <= 0)
+            {
+                hitPoint = 0;
+                Death();
+            }
+        }
+    }
+
+    protected virtual void Death()
+    {
+        
+    }
+}
