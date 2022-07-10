b40d3af8decf672e7d3653e30bfdcc511f95eed8
Nikita <shpulinn@mail.ru>
2022-07-10 13:50:51 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : Mover
 {
+    private SpriteRenderer _spriteRenderer;
+
+    protected override void Start()
+    {
+        base.Start();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         UpdateMotor(new Vector3(x, y, 0));
+    }
+
+    public void SwapSprite(int spriteID)
+    {
+        _spriteRenderer.sprite = GameManager.instance.playerSprites[spriteID];
     }
 }
