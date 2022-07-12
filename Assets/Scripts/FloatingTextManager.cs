5e6089bc705368ab6d9d1dda74bfb6253d744aec
Nikita <shpulinn@mail.ru>
2022-07-12 19:23:53 +0200

    Update FloatingTextManager.cs

--- Assets/Scripts/FloatingTextManager.cs
+++ Assets/Scripts/FloatingTextManager.cs
@@ -12,11 +12,6 @@ public class FloatingTextManager : MonoBehaviour
 
     private List<FloatingText> _floatingTexts = new List<FloatingText>();
 
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
     private void Update()
     {
         foreach (FloatingText txt in _floatingTexts)
