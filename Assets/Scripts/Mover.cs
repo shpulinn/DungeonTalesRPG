f12da8fd8853d4b0e8769e2c4f16bb9a1672bf77
Nikita <shpulinn@mail.ru>
2022-07-13 17:19:54 +0200

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,8 +7,8 @@ public abstract class Mover : Fighter
     protected BoxCollider2D _boxCollider;
     protected Vector3 _moveDelta;
     protected RaycastHit2D _hit;
-    protected float ySpeed = 0.75f;
-    protected float xSpeed = 1.0f;
+    public float ySpeed = 0.75f;
+    public float xSpeed = 1.0f;
 
     protected virtual void Start()
     {
