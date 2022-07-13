f0ea43f69eef84a33845c66f0e49e92e572e0b5e
Nikita <shpulinn@mail.ru>
2022-07-13 17:19:58 +0200

    Update Fighter.cs

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -25,7 +25,7 @@ public class Fighter : MonoBehaviour
             healthPoint -= dmg.damageAmount;
             pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
             
-            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 25, Color.red, transform.position, Vector3.zero, 0.5f);
+            GameManager.instance.ShowText(dmg.damageAmount.ToString(), 15, Color.red, transform.position, Vector3.zero, 0.5f);
 
             if (healthPoint <= 0)
             {
